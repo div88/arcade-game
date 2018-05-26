1fec0dca660b3db796a7516821cdbdd97491c296
Divya Iyengar <divyaiyengar@divyas-mbp.attlocal.net>
2018-05-26 16:18:17 -0700

    Class and comments added(review)

--- js/app.js
+++ js/app.js
@@ -1,18 +1,31 @@
-// Enemies our player must avoid
-var Enemy = function(x,y) {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
+class Character {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+    }
+}
 
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
-    this.sprite = 'images/enemy-bug.png';
-    this.x = x;
-    this.y = y;
-    this.width = 101;
-    this.height = 171;
-    this.initialLocation = (this.x, this.y);
-    this.speed = 160;
+class Enemy extends Character {
+    constructor(x,y) {
+        super(x, y);
+        this.sprite = 'images/enemy-bug.png';
+        this.initialLocation = (this.x, this.y);
+        this.speed = Math.floor((Math.random() * 50) + 100);
+    }
+}
+
+// Now write your own player class
+// This class requires an update(), render() and
+// a handleInput() method.
+class Player extends Character {
+    constructor(x,y) {
+        super(x, y);
+        this.sprite = 'images/char-boy.png';
+        this.initialLocation = (this.x, this.y);
+        this.speed = Math.floor((Math.random() * 100) + 100);
+    }
 };
+
 
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
@@ -22,6 +35,7 @@ Enemy.prototype.update = function(dt) {
     // all computers
     this.x += this.speed * dt;
     
+    //Once enemy reaches the end of canvas, it will restart from the other end
     if(this.x >= 500){
         this.x = -50;
     }
@@ -32,22 +46,7 @@ Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
-// Now write your own player class
-// This class requires an update(), render() and
-// a handleInput() method.
-var Player = function(x,y,speed) {
-    this.sprite = 'images/char-boy.png';
-    this.x = x;
-    this.y = y;
-    this.width = 101;
-    this.height = 171;
-    this.initialLocation = (this.x, this.y);
-    this.speed = speed;
-};
 
-Player.prototype.update = function() {
-    
-};
 
 Player.prototype.win = function() {
     console.log("You Won!!!!");
@@ -57,22 +56,29 @@ Player.prototype.render = function() {
    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
+Player.prototype.update = function() {
+ };
+
 //Keeps the player inside the canvas
 Player.prototype.handleInput = function(keyPressed) {
     
+    //Gets back player to center, if it reaches right end of canvas
     if(this.x >= 410){
         this.x = 200;
     }
 
+    //Gets back player to center, if it reaches left end of canvas
     if(this.x <= -10){
         this.x = 200;
         //470
     }
 
+    //Gets back player to starting point, if it reaches bottom end of canvas
     if(this.y >= 440){
         this.y = 370;
     }
 
+    //Gets back player to starting point, if it reaches top end of canvas
     if(this.y <= 0){
         this.y = 0;
         //400
@@ -80,21 +86,17 @@ Player.prototype.handleInput = function(keyPressed) {
 
    
     if(keyPressed == "up" || keyPressed == "down"){
-        
             if(keyPressed == "up"){
                 this.y = this.y - 10;
             } else if(keyPressed == "down"){
                 this.y = this.y + 10;
             }
-        console.log(this.y);
-       
     } else if(keyPressed == "left" || keyPressed == "right"){
         if(keyPressed == "right"){
             this.x = this.x + 10;
         } else if(keyPressed == "left" && this.x <= 420){
             this.x = this.x - 10;
         }
-        // console.log(this.x);
     }
 };
 
@@ -109,9 +111,9 @@ Player.prototype.onWin = function(){
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
 
-var enemy1 = new Enemy(0,230,80);
-var enemy2 = new Enemy(200,145,50);
-var enemy3 = new Enemy(0,60,20);
+var enemy1 = new Enemy(0,230);
+var enemy2 = new Enemy(200,145);
+var enemy3 = new Enemy(0,60);
 
 var allEnemies = [enemy1, enemy2, enemy3];
 var player = new Player(200,400,100);
